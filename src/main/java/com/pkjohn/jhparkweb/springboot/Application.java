ba23418708723f43c1267c89b530b860fa831eb3
pkjohn91 <pkjohn91@gmail.com>
2022-02-03 01:58:19 +0900

    404 errors

--- src/main/java/com/pkjohn/jhparkweb/springboot/Application.java
+++ src/main/java/com/pkjohn/jhparkweb/springboot/Application.java
@@ -2,6 +2,7 @@ package com.pkjohn.jhparkweb.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing // jpa auditing 어노테이션들을 모두 활성화할 수 있도록 함.
